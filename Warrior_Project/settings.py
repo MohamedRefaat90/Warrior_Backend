1a0b45b2726c23beedb4cdb7d943a7f5386fe638
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-12-07 05:46:35 +0200

    - change to mysql database

--- Warrior_Project/settings.py
+++ Warrior_Project/settings.py
@@ -110,13 +110,12 @@ WSGI_APPLICATION = 'Warrior_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': '5432',
+        'PORT': '3306',
     }
 }
 
