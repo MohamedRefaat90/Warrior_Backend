22ae1a01f06071e82cbe1f6b7073194f3260b1d8
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-12-12 21:00:32 +0200

    Update deployment script to regenerate requirements.txt before installing dependencies

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 cd /home/mohamedrefaat90/Warrior_Backend  
 git pull origin main
 source /home/mohamedrefaat90/.virtualenvs/Warrior/bin/activate  # Activate virtual environment
+pip freeze > requirements.txt
 pip install -r requirements.txt             
 python manage.py makemigrations                    
 python manage.py migrate                     
